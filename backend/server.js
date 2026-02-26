0f73d1267c43751934a3cfc7102c63e7202b32a8
Jack Kennedy <jkennedy7555@gmail.com>
2026-02-26 15:24:25 -0500

    Add bossNames

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,17 @@ function mapByKey(rows, key) {
   return map;
 }
 
+const bossNames = [
+  "Abyssal Sire","Alchemical Hydra","Barrows Chests","Bryophyta","Callisto",
+  "Cerberus","Chambers of Xeric","Chambers of Xeric Challenge Mode","Chaos Elemental","Chaos Fanatic",
+  "Commander Zilyana","Corporeal Beast","Crazy Archaeologist","Dagannoth Prime","Dagannoth Rex",
+  "Dagannoth Supreme","Deranged Archaeologist","General Graardor","Giant Mole","Grotesque Guardians",
+  "Hespori","Kalphite King","King Black Dragon","Kraken","Kree'Arra","K'ril Tsutsaroth",
+  "Mimic","Nightmare","Obor","Phosani's Nightmare","Sarachnis","Scorpia","Skotizo","The Gauntlet",
+  "The Corrupted Gauntlet","Theatre of Blood","Theatre of Blood Hard Mode","Thermonuclear Smoke Devil",
+  "TzKal-Zuk","TzTok-Jad","Venenatis","Vet'ion","Vorkath","Wintertodt","Zalcano","Zulrah"
+];
+
 function computeSkillDiffs(current, previous = {}) {
   const result = {};
   let changed = false;
